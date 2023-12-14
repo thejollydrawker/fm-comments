99eb228f0321e7d73be7e5dd5f99ecb55fef5788
thejollydrawker <rekaketezeregy@gmail.com>
2023-12-14 17:49:03 +0100

    adding replies and score

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,13 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { Comment } from '../../models/comment.model';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
 export class CommentComponent {
   @Input({ required: true }) comment!: Comment;
+
+  openReply: number = 0;
+
+  score(comment:Comment): void {
+    comment.score += 1;
+  }
+
+  unscore(comment: Comment) {
+    if (comment.score > 0) {
+      comment.score -= 1;
+    }
+  }
 }
