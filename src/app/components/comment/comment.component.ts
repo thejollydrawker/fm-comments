5134fb7487dabb372f8e91e769688e24f3d0fb9a
thejollydrawker <rekaketezeregy@gmail.com>
2023-12-18 16:54:03 +0100

    adding modal, button styles

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, signal } from '@angular/core';
 import { Comment } from '../../models/comment.model';
-import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+import { AsyncPipe, NgIf, NgTemplateOutlet } from '@angular/common';
 import { CommentsService } from '../../services/comments.service';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [NgTemplateOutlet, AsyncPipe],
+  imports: [NgTemplateOutlet, AsyncPipe, NgIf, ModalComponent],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
@@ -14,6 +15,8 @@ export class CommentComponent {
   @Input({ required: true }) comment!: Comment;
 
   openReply: number = 0;
+  editComment: number = 0;
+  openModal: boolean = false;
 
   currentUser$ = this.commentSrv.getUser();
 
